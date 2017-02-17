d7a49d199803f0335c0a1893e062e25a3758ef91
Tomoyuki Fujimori <moyu@dromozoa.com>
2017-02-17 14:52:19 +0900

    リファクタリング。

--- httppost_handle.cpp
+++ httppost_handle.cpp
@@ -15,7 +15,6 @@
 // You should have received a copy of the GNU General Public License
 // along with dromozoa-curl.  If not, see <http://www.gnu.org/licenses/>.
 
-#include <iostream>
 #include <vector>
 
 #include "common.hpp"
